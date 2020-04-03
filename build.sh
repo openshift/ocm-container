9cafc9e105978be1c526f8ebf84e728eaf82e9cb
Drew Anderson <dranders@redhat.com>
2020-04-03 13:29:03 +0200

    fail if osv4client isn't provided

--- build.sh
+++ build.sh
@@ -5,13 +5,15 @@ if [ ! -f ./.git/config ]; then
     exit 1;
 fi
 
-echo "Check the following URL for latest available OpenShift client:"
-echo "https://mirror.openshift.com/pub/openshift-v4/clients/ocp/latest/"
-echo
-echo "export osv4client=openshift-client-linux-4.3.5.tar.gz"
-echo ${0}
-echo
-echo
+if [ "x${osv4client}" == "x" ]; then
+    echo "Check the following URL for latest available OpenShift client:"
+    echo "https://mirror.openshift.com/pub/openshift-v4/clients/ocp/latest/"
+    echo
+    echo "export osv4client=openshift-client-linux-4.3.5.tar.gz"
+    echo ${0}
+    echo
+    exit 1
+fi
 
 time sudo docker build --no-cache \
   --build-arg osv4client=${osv4client} \
