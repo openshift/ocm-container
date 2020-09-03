3dddff1a5b7445b151e185da86bb5b6c6f87abf6
Drew Anderson <dranders@redhat.com>
2020-09-03 14:52:17 +0200

    add ~/.ssh and agent for bouncing through machines

--- ocm-container.sh
+++ ocm-container.sh
@@ -16,9 +16,13 @@ fi
 source ${OCM_CONTAINER_CONFIG}
 
 ### start container
-${CONTAINER_SUBSYS} run -it --rm \
+${CONTAINER_SUBSYS} run -it --rm --privileged \
 -e "OFFLINE_ACCESS_TOKEN=${OFFLINE_ACCESS_TOKEN}" \
 -e "CLI=${CLI}" \
 -e "OCM_USER=${OCM_USER}" \
 -e "OCM_URL=${OCM_URL}" \
+-e "SSH_AUTH_SOCK=/tmp/ssh.sock" \
+-v ${SSH_AUTH_SOCK}:/tmp/ssh.sock \
+-v ${HOME}/.ssh:/root/.ssh \
 ocm-container /bin/bash ## -c "/container-setup/login.sh $@ && /container-setup/bash-ps1-wrap.sh"
+
