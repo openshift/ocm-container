ae2cbc571570e0fb3c1843862e5fb4717ba65209
Ron Green <11993626+georgettica@users.noreply.github.com>
2020-09-06 10:47:26 +0300

    fix(ocm-container.sh): docker absolute path
    the error:
    
    'docker: Error response from daemon: create ./env.source: \
    ./env.source includes invalid characters for a local volume name, \
    only [a-zA-Z0-9][a-zA-Z0-9_.-] are allowed. \
    If you intended to pass a host directory, use absolute path.'
    
    requires an absolute path for running 'ocm-container.sh'

--- ocm-container.sh
+++ ocm-container.sh
@@ -19,7 +19,7 @@ source ${OCM_CONTAINER_CONFIG}
 ${CONTAINER_SUBSYS} run -it --rm --privileged \
 -e "OCM_URL=${OCM_URL}" \
 -e "SSH_AUTH_SOCK=/tmp/ssh.sock" \
--v ./env.source:/root/env.source \
+-v $(pwd)/env.source:/root/env.source \
 -v ${SSH_AUTH_SOCK}:/tmp/ssh.sock \
 -v ${HOME}/.ssh:/root/.ssh \
 ocm-container /bin/bash ## -c "/container-setup/login.sh $@ && /container-setup/bash-ps1-wrap.sh"
