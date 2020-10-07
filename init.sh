dd48107e4ad490f0783a17493cfb3890c1068b72
Kirk Bater <kirk.bater@gmail.com>
2020-10-07 11:29:30 -0400

    Adds migration for existing configs.

--- init.sh
+++ init.sh
@@ -1,27 +1,59 @@
 #!/bin/bash
 
 cd $(dirname $0)
+CONFIG_DIR=${HOME}/.config/ocm-container
 
-CONFIG_DIR=${HOME}/.config/ocm-env
+function init_new_config() {
+  if [ ! -f ${CONFIG_DIR}/env.source ]
+  then
+    echo "Initializing Default Configuration"
+    cp env.source.sample ${CONFIG_DIR}/env.source
+  else
+    echo "ocm-container is already configured."
+  fi
+}
+
 mkdir -p ${CONFIG_DIR}
 
-if [ ! -f ${CONFIG_DIR}/env.source ]
+if [ -f env.source ]
 then
-  echo "Initializing Default Configuration"
-  cp env.source.sample ${CONFIG_DIR}/env.source
+  echo "We see you already have a local configuration file.  Would you like us to:"
+  echo "  1: Move the config file to the new config location"
+  echo "  2: Symlink the config file to the new config location"
+  echo "  3: Create a new config file at the new config location"
+  echo "  4: Do nothing and exit"
+  read -n 1 -p "Select 1-4: " prev_config_selection
+  echo
+
+  if [ $prev_config_selection == "1" ]
+  then
+    echo "Moving configuration file"
+    mv env.source ${CONFIG_DIR}
+  elif [ $prev_config_selection == "2" ]
+  then
+    echo "Symlinking configuration file"
+    ln -s env.source $CONFIG_DIR/env.source
+  elif [ $prev_config_selection == "3" ]
+  then
+    init_new_config
+  else
+    echo "Exiting."
+    exit 0
+  fi
 else
-  echo "ocm-env is already configured."
+  init_new_config
 fi
 
-echo "Creating symlink"
-ln -s "$(pwd)/ocm-container.sh" /usr/local/bin/ocm-env
+echo "Creating symlink for ocm-container binary"
+ln -sfn "$(pwd)/ocm-container.sh" /usr/local/bin/ocm-container
 
 echo
 echo "Tip: Many developers like to add the following alias:"
-echo "alias ocm-env-stg=\"OCM_URL=staging ocm-env\""
+echo "alias ocm-container-stg=\"OCM_URL=staging ocm-container\""
 
 echo
 echo
-echo "ocm-env configuration can be customized by editing ${CONFIG_DIR}/env.source"
+echo "ocm-container configuration can be customized by editing ${CONFIG_DIR}/env.source"
+
 
 
