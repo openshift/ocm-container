050af02166a3713946639bf17be56b3096a29734
Karthik Perumal <kramraja@redhat.com>
2021-01-29 08:16:07 +1000

    Make default velero ns FizzBuzzEnterprise compliant

--- container-setup/utils/bashrc_supplement.sh
+++ container-setup/utils/bashrc_supplement.sh
@@ -19,7 +19,7 @@ complete -C '/usr/local/aws/aws/dist/aws_completer' aws
 
 ### Set the default namespace for velero to avoid using 
 ### --namespace=openshift-velero each time
-velero client config set namespace=openshift-velero
+velero client config set namespace=${DEFAULT_VELERO_NS}
 
 if [ -n "$INITIAL_CLUSTER_LOGIN" ]
 then
