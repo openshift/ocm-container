ab91ad3604982a782b11db44bf8053b3dac4f0c0
Kirk Bater <kirk.bater@gmail.com>
2024-03-28 09:06:15 -0400

    removes arm64 options from pr check and automated build

--- .ci/release-build.sh
+++ .ci/release-build.sh
@@ -6,10 +6,10 @@ set -euo pipefail
 ./.ci/build.sh
 
 make TAG=latest-amd64 ARCHITECTURE=amd64 tag
-make TAG=latest-arm64 ARCHITECTURE=arm64 tag
+# make TAG=latest-arm64 ARCHITECTURE=arm64 tag
 
 make TAG=latest-amd64 ARCHITECTURE=amd64 push
-make TAG=latest-arm64 ARCHITECTURE=arm64 push
+# make TAG=latest-arm64 ARCHITECTURE=arm64 push
 
 make build-manifest
 make push-manifest
