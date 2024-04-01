d69ed71b3a7bc43ce875a6ea7805b02eb7ec915b
Chris Collins <collins.christopher@gmail.com>
2024-04-01 12:24:59 -1000

    better deprecation message parsing
    Signed-off-by: Chris Collins <collins.christopher@gmail.com>

--- pkg/deprecation/deprecation.go
+++ pkg/deprecation/deprecation.go
@@ -8,10 +8,10 @@ import (
 func Message(deprecated string, alternative ...interface{}) {
 	var s strings.Builder
 
-	s.WriteString(fmt.Sprintf("WARNING: %s is deprecated and will be removed in a future release.", deprecated))
+	s.WriteString(fmt.Sprintf("WARNING: '%s' is deprecated and will be removed in a future release.", deprecated))
 	if len(alternative) > 0 {
 		// Space before "Please" is intentional
-		s.WriteString(fmt.Sprintf(" Please use %s instead", alternative[:]))
+		s.WriteString(fmt.Sprintf(" Please use '%s' instead", alternative[:]...))
 	}
 
 	fmt.Println(s.String())
