41879b2e9009cc2901fe94e5922862687b0bf24c
Kirk Bater <kirk.bater@gmail.com>
2024-05-13 10:20:55 -0400

    Updates tests to be able to expect multiple parameters

--- pkg/engine/engine_test.go
+++ pkg/engine/engine_test.go
@@ -57,12 +57,22 @@ func TestParseRefToArgs(t *testing.T) {
 	testCases := []struct {
 		name      string
 		container ContainerRef
-		expected  string
+		expected  []string
 	}{
 		{
 			name:      "Tests Publish All",
 			container: ContainerRef{PublishAll: true},
-			expected:  "--publish-all",
+			expected:  []string{"--publish-all"},
+		},
+		{
+			name:      "Tests LocalPorts single",
+			container: ContainerRef{LocalPorts: map[string]int{"console": 9999}},
+			expected:  []string{"--publish=127.0.0.1::9999"},
+		},
+		{
+			name:      "Tests LocalPorts multiple",
+			container: ContainerRef{LocalPorts: map[string]int{"console": 9999, "promlens": 8080}},
+			expected:  []string{"--publish=127.0.0.1::9999", "--publish=127.0.0.1::8080"},
 		},
 	}
 
@@ -77,18 +87,47 @@ func TestParseRefToArgs(t *testing.T) {
 		}
 	})
 
+	// run once for special empty local port map case
+	t.Run("Tests empty LocalPorts map returns no args", func(t *testing.T) {
+		args, err := parseRefToArgs(ContainerRef{LocalPorts: map[string]int{}})
+		if err != nil {
+			t.Errorf("Unexpected Error: %v", err)
+		}
+		if len(args) > 0 {
+			t.Errorf("Expected empty arg slice, got len %d :: %v", len(args), args)
+		}
+	})
+
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
 			args, err := parseRefToArgs(tc.container)
 			if err != nil {
 				t.Errorf("Expected no error but got %v", err)
 			}
+
+			// create the map to determine if all have been found
+			expectedFound := map[string]bool{}
+			for _, flag := range tc.expected {
+				expectedFound[flag] = false
+			}
+
+			// loop through all args and mark as found
 			for _, arg := range args {
-				if arg == tc.expected {
+				for _, expected := range tc.expected {
+					if arg == expected {
+						expectedFound[expected] = true
+					}
+				}
+			}
+
+			// loop through all of the expected to be found args and if
+			// not all are true then return an error
+			for _, found := range expectedFound {
+				if !found {
+					t.Errorf("%s not found in args: %v", tc.expected, args)
 					return
 				}
 			}
-			t.Errorf("%s not found in args: %v", tc.expected, args)
 		})
 	}
 }
