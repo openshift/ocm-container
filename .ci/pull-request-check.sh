02d50e694b89fca0b9a7772a6388b911463ae360
Chris Collins <collins.christopher@gmail.com>
2024-07-08 13:37:05 -1000

    Adds ability to use GITHUB_TOKEN auth for backplane-tools
    Adds the ability to use an exported `GITHUB_TOKEN` env variable for
    backplane-tools authenticated installs.
    
    Signed-off-by: Chris Collins <collins.christopher@gmail.com>

--- .ci/pull-request-check.sh
+++ .ci/pull-request-check.sh
@@ -2,8 +2,10 @@
 
 set -euo pipefail
 
+GITHUB_TOKEN="${GITHUB_TOKEN:-}"
+
 # Build the images
-make BUILD_ARGS="--no-cache" build-image-amd64
+make BUILD_ARGS="--no-cache --build-arg GITHUB_TOKEN=${GITHUB_TOKEN}" build-image-amd64
 # make BUILD_ARGS="--no-cache" build-image-arm64
 
 make TAG=latest-amd64 ARCHITECTURE=amd64 tag
