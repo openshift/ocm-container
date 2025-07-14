1f43e7752fd5694244ea4855ff61086ef5ed70c2
Chris Collins <collins.christopher@gmail.com>
2025-07-14 13:36:55 -1000

    Replace complicated RUN commands with python script
    This replaces the long and complicated RUN commands that would curl multiple github urls to download packages not installed by backplane-tools with a python script that handles all the logic for downloading the binary and checksums and validating the binary and checksums match.
    
    Signed-off-by: Chris Collins <collins.christopher@gmail.com>

--- .ci/pull-request-check.sh
+++ .ci/pull-request-check.sh
@@ -5,8 +5,5 @@ set -euo pipefail
 GITHUB_TOKEN="${GITHUB_TOKEN:-}"
 
 # Build the images
-make BUILD_ARGS="--no-cache --build-arg GITHUB_TOKEN=${GITHUB_TOKEN}" build-image-amd64
+make BUILD_ARGS="--build-arg GITHUB_TOKEN=${GITHUB_TOKEN}" build-image-amd64
 # make BUILD_ARGS="--no-cache" build-image-arm64
-
-make TAG=latest-amd64 ARCHITECTURE=amd64 tag
-# make TAG=latest-arm64 ARCHITECTURE=arm64 tag
