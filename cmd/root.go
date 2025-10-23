e771af9556168c6b11582629bed85a7fc383cae6
Kirk Bater <kirk.bater@gmail.com>
2025-10-23 16:34:48 -0400

    set podman as default engine (#412)

--- cmd/root.go
+++ cmd/root.go
@@ -225,6 +225,7 @@ func initConfig() {
 	}
 
 	viper.SetEnvPrefix(programPrefix)
+	viper.SetDefault("engine", "podman")
 	viper.AutomaticEnv() // read in environment variables that match
 
 	// If a config file is found, read it in.
